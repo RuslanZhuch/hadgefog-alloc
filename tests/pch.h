c3b5438c8d4f4e29833d2ba9ac8bbed84b757035
RuslanZhuch <ruslanacs93@mail.ru>
2022-02-25 00:09:23 +0300

    Sollution params and gtests

---
+++ tests/pch.h
@@ -0,0 +1,20 @@
+//
+// pch.h
+//
+
+#pragma once
+
+#pragma warning(disable : 5050)
+
+#pragma warning( push )
+
+#pragma warning(disable : 4668)
+#pragma warning(disable : 4820)
+#pragma warning(disable : 4625)
+#pragma warning(disable : 4514)
+#pragma warning(disable : 4626)
+
+
+#include "gtest/gtest.h"
+
+#pragma warning( pop )
