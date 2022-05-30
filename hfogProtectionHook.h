7650cdf5d9221096e70ef7454677de53538be5a5
RuslanZhuch <ruslanacs93@mail.ru>
2022-05-31 00:13:00 +0300

    Default protection update

--- hfogProtectionHook.h
+++ hfogProtectionHook.h
@@ -6,7 +6,7 @@
 import trampa.key;
 import hfog.Protect;
 
-[[nodiscard]] bool HfogHook::onAssertTriggered([[maybe_unused]] const trampa::Assert::Cause& assertCause, std::string_view msg)
+[[nodiscard]] bool HfogHook::onAssertTriggered([[maybe_unused]] const trampa::Assert::Cause& assertCause, [[maybe_unused]] std::string_view msg)
 {
 
 	assert(msg.data());
